cc0a034b50197f2307b82d22d1239e789e640758
VasylBarna <vasylbarna@gmail.com>
2021-08-19 15:44:27 +0300

    add markup

---
+++ src/Loader/Loader.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import Loader from 'react-loader-spinner';
+
+export default class App extends Component {
+  //other logic
+  render() {
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    );
+  }
+}
